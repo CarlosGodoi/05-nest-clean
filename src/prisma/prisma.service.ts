863c28cc9cc181a8a28f8c464aada0737dd6d58f
carlosgodoi <edu-godoi@hotmail.com>
2026-06-03 17:12:10 -0300

    feat: criando prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client.js";
+
+@Injectable()
+export class PrismaService
+ extends PrismaClient 
+ implements OnModuleInit, OnModuleDestroy 
+ {
+
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    })
+    super({ adapter, log: ['warn', 'error'] })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
